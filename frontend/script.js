c11cee5923bdcf84f318242b38ea597f2bfc2b56
Arthur Vinicius <119056450+7thur@users.noreply.github.com>
2026-09-21 21:22:55 -0300

    Adiciona interação JS do login com fetch

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,24 @@
 console.log("Frontend carregado");
+
+const usuario = document.getElementById("Username");
+const senha = document.getElementById("senha");
+const form = document.querySelector("form");
+const mensagem = document.getElementById("mensagem");
+
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  console.log("Formulário enviado");
+
+const response = await fetch("http://localhost:8080/login", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ username: usuario.value, password: senha.value })
+});
+const dados = await response.json();
+console.log(dados);
+mensagem.textContent = "Algum texto...";
+});
+
+
+
